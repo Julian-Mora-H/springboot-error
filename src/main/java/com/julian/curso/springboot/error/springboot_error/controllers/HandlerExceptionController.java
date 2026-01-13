6119725042f84e633962893266ed39d049c85fe1
Julian-Mora <felipmora43@gmail.com>
2026-01-13 18:20:00 -0500

    Personalizacion de Error 404 y como desactivar la excepcion por default de Spring a este error por el application.properties

--- src/main/java/com/julian/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/julian/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -6,13 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.julian.curso.springboot.error.springboot_error.models.Error;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
 
-    @ExceptionHandler({ArithmeticException.class})
+    @ExceptionHandler({ArithmeticException.class}) // Captura error division por cero
     public ResponseEntity<Error> divisionByZero(Exception ex){
         Error error = new Error();
         error.setDate(new Date());
@@ -26,5 +27,16 @@ public class HandlerExceptionController {
         // Una forma de dar mensaje de excepcion con ResponseEntity y propiedad internalServerError
         //return ResponseEntity.internalServerError().body(error);
         
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class) // Captura error 404
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e){
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Api rest no encontrado!");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
 }
